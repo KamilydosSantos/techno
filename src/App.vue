dcdfe8b4efc8f58915499aa6a9a0a8d02a776999
kamily <kamily7803@gmail.com>
2023-06-12 14:29:18 -0300

    feat: add css styling and correct nomenclature

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,14 @@
 <template>
   <div id="app">
-    {{ produtos }}
+    <section class="products">
+      <div v-for="product in products" :key="product.id" class="products__product">
+        <img :src="product.img" :alt="product.nome" class="products__product__img">
+        <div class="products__product__info">
+          <span class="products__product__info__price">{{ product.price }}</span>
+          <h2 class="products__product__info__title">{{ product.name }}</h2>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
@@ -10,26 +18,73 @@ export default {
   name: 'App',
   data() {
     return {
-      produtos: []
+      products: []
     };
   },
   methods: {
-    fetchProdutos() {
-      fetch("/api/produtos.json")
+    fetchProducts() {
+      fetch("/api/products.json")
         .then(response => response.json())
         .then(response => {
-          this.produtos = response;
+          this.products = response;
         });
     },
   },
   created() {
-    this.fetchProdutos();
+    this.fetchProducts();
   },
 }
 </script>
 
 <style lang="scss">
-#app {
+@import url('https://fonts.googleapis.com/css2?family=Noto+Serif:wght@400;700&display=swap');
 
+* {
+  box-sizing: border-box;
+}
+
+body{
+  margin: 0;
+  padding: 0;
+  background: linear-gradient(to right, #1a1a1a 30%, #ffffff 30%);
+}
+
+#app {
+  font-family: Noto serif;
+  padding: 0 80px;
+  h1,
+  h2, 
+  ul,
+  li {
+    padding: 0;
+    margin: 0;
+  }
+  ul {
+    list-style: none;
+  }
+  .products {
+    max-width: 900px;
+    margin: 100px auto 0 auto;
+    .products__product {
+      display: flex;
+      align-items: center;
+      margin-bottom: 40px;
+      background: #ffffff;
+      box-shadow: 0 0 2rem rgba(0, 0, 0, .1);
+      .products__product__img {
+        max-width: 300px;
+        margin-right: 40px;
+      }
+      .products__product__info {
+        .products__product__info__title {
+          font-size: 3rem;
+          line-height: 1;
+        }
+        .products__product__info__price {
+          color: rgba(0, 0, 0, .5);
+        }
+      }
+    }
+  }
 }
 </style>
